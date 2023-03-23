4e8a6066e4758e65d2b7ec92e152e0b9c3389ed2
Alba Garza <jarazethalba@gmail.com>
2023-03-22 23:22:41 -0600

    class component simple state

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
+
 import './App.css';
 
-function App() {
+class App extends Component{
+
+ //says hey whenever this app is instantiated run this first
+  constructor(){
+    super();
+
+    this.state = {
+      name: 'Yihua'
+    };
+
+  }
+
+  render(){ 
   return (
     <div className="App">
-    <h1>Initialized app....Test tes</h1>
+    <h1>This is a demo...{this.state.name}</h1>
+    <button onClick={()=> {
+      //give it an obj that you want to shallow merge, checks keys
+      this.setState({name: 'Andrei'});
+    }}
+    >Change Name</button>
     </div>
   );
+  }
 }
 
 export default App;
